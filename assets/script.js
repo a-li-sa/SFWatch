f754069aea212c63ff06bd838acdfef7af2eaf61
Ian Panlilio <ipanlilio7@gmail.com>
2020-08-23 11:50:30 -0700

    added COVID test sites

--- assets/script.js
+++ assets/script.js
@@ -568,107 +568,305 @@ $(document).ready(function () {
     });
   }
 
-  var policeStationArray = [
-    {
-      stationName: "Bayview Police Station",
-      address: "201 Williams Ave, San Francisco, CA 94124",
-      phone: "415-671-2300",
-      abbr: "bayviewStation",
-      lng: 37.729664,
-      lat: -122.398086,
-    },
-    {
-      stationName: "Central Police Station",
-      address: "766 Vallejo St, San Francisco, CA 94133",
-      phone: "415-315-2400",
-      abbr: "centralStation",
-      lng: 37.798645,
-      lat: -122.409862,
-    },
-    {
-      stationName: "Ingleside Police Station",
-      address: "1 Sgt John V Young Ln, San Francisco, CA 94112",
-      phone: "415-404-4000",
-      abbr: "inglesideStation",
-      lng: 37.72477,
-      lat: -122.446225,
-    },
-    {
-      stationName: "Mission Police Station",
-      address: "630 Valencia St, San Francisco, CA 94110",
-      phone: "415-558-5400",
-      abbr: "missionStation",
-      lng: 37.76268,
-      lat: -122.421969,
-    },
-    {
-      stationName: "Northern Police Station",
-      address: "1125 Filmore St, San Francisco, CA 94110",
-      phone: "415-614-3400",
-      abbr: "northernStation",
-      lng: 37.780161,
-      lat: -122.43239,
-    },
-    {
-      stationName: "Park Police Station",
-      address: "1899 Waller St, San Francisco, CA 94158",
-      phone: "415-242-3000",
-      abbr: "parkStation",
-      lng: 37.767848,
-      lat: -122.455266,
-    },
-    {
-      stationName: "Richmond Police Station",
-      address: "461 6th Ave, San Francisco, CA 94118",
-      phone: "415-666-8000",
-      abbr: "richmondStation",
-      lng: 37.780001,
-      lat: -122.46446,
-    },
-    {
-      stationName: "Southern Police Station",
-      address: "850 Bryant St, San Francisco, CA 94116",
-      phone: "415-553-1373",
-      abbr: "southernStation",
-      lng: 37.774988,
-      lat: -122.404328,
-    },
-    {
-      stationName: "Taraval Police Station",
-      address: "2345 24th Ave, San Francisco, CA 94116",
-      phone: "415-759-3100",
-      abbr: "taravalStation",
-      lng: 37.743733,
-      lat: -122.48146,
-    },
-    {
-      stationName: "Tenderloin Police Station",
-      address: "301 Eddy St, San Francisco, CA 94102",
-      phone: "415-345-7300",
-      abbr: "tenderloinStation",
-      lng: 37.783704,
-      lat: -122.412895,
-    },
-  ];
+// Police Station Code
 
-  const policeColor = 'blue';
+var policeStationArray = [
+  {
+    stationName: "Bayview Police Station",
+    address: "201 Williams Ave, San Francisco, CA 94124",
+    phone: "415-671-2300",
+    abbr: "bayviewStation",
+    lng: 37.729664,
+    lat: -122.398086,
+  },
+  {
+    stationName: "Central Police Station",
+    address: "766 Vallejo St, San Francisco, CA 94133",
+    phone: "415-315-2400",
+    abbr: "centralStation",
+    lng: 37.798645,
+    lat: -122.409862,
+  },
+  {
+    stationName: "Ingleside Police Station",
+    address: "1 Sgt John V Young Ln, San Francisco, CA 94112",
+    phone: "415-404-4000",
+    abbr: "inglesideStation",
+    lng: 37.72477,
+    lat: -122.446225,
+  },
+  {
+    stationName: "Mission Police Station",
+    address: "630 Valencia St, San Francisco, CA 94110",
+    phone: "415-558-5400",
+    abbr: "missionStation",
+    lng: 37.76268,
+    lat: -122.421969,
+  },
+  {
+    stationName: "Northern Police Station",
+    address: "1125 Filmore St, San Francisco, CA 94110",
+    phone: "415-614-3400",
+    abbr: "northernStation",
+    lng: 37.780161,
+    lat: -122.43239,
+  },
+  {
+    stationName: "Park Police Station",
+    address: "1899 Waller St, San Francisco, CA 94158",
+    phone: "415-242-3000",
+    abbr: "parkStation",
+    lng: 37.767848,
+    lat: -122.455266,
+  },
+  {
+    stationName: "Richmond Police Station",
+    address: "461 6th Ave, San Francisco, CA 94118",
+    phone: "415-666-8000",
+    abbr: "richmondStation",
+    lng: 37.780001,
+    lat: -122.46446,
+  },
+  {
+    stationName: "Southern Police Station",
+    address: "850 Bryant St, San Francisco, CA 94116",
+    phone: "415-553-1373",
+    abbr: "southernStation",
+    lng: 37.774988,
+    lat: -122.404328,
+  },
+  {
+    stationName: "Taraval Police Station",
+    address: "2345 24th Ave, San Francisco, CA 94116",
+    phone: "415-759-3100",
+    abbr: "taravalStation",
+    lng: 37.743733,
+    lat: -122.48146,
+  },
+  {
+    stationName: "Tenderloin Police Station",
+    address: "301 Eddy St, San Francisco, CA 94102",
+    phone: "415-345-7300",
+    abbr: "tenderloinStation",
+    lng: 37.783704,
+    lat: -122.412895,
+  },
+];
 
-  $('#policeStationButton').on('click', function () {
-    for (let i = 0; i < policeStationArray.length; i++) {
-      if (policeStationArray.stationName !== '') {
-        let lat = policeStationArray[i].lat;
-        let lon = policeStationArray[i].lng;
-        let policeStation = policeStationArray[i].stationName
-        let policeAddress = policeStationArray[i].address;
-        let policePhone = policeStationArray[i].phone;
-        var marker = L.marker([lon, lat]).addTo(mymap1).addTo(mymap1).bindPopup(`
-        <strong>${policeStation}</strong>
-        <br>
-        ${policeAddress}
-        <br>
-        ${policePhone}
-      `);
-      }
+const policeColor = 'blue';
+
+$('#policeStationButton').on('click', function () {
+  for (let i = 0; i < policeStationArray.length; i++) {
+    if (policeStationArray.stationName !== '') {
+      let lat = policeStationArray[i].lat;
+      let lon = policeStationArray[i].lng;
+      let policeStation = policeStationArray[i].stationName
+      let policeAddress = policeStationArray[i].address;
+      let policePhone = policeStationArray[i].phone;
+      var marker = L.marker([lon, lat]).addTo(mymap1).bindPopup(`
+      <strong>${policeStation}</strong>
+      <br>
+      ${policeAddress}
+      <br>
+      ${policePhone}
+    `);
     }
-  });
+  }
+});
+});
+
+// COVID Code
+
+var covidArray = [
+  {
+    siteName: "UCSF Laurel Heights Campus",
+    address: "3333 California St, San Francisco, CA 94118",
+    phone: "415-476-8868",
+    abbr: "ucsflaurelheights",
+    appointment: "Required",
+    referral: "Required",
+    testing: "Tests Limited to Certain Patients",
+    drive: "Drive-Through Allowed",
+    lng: 37.786360,
+    lat: -122.448440,
+  },
+  {
+    siteName: "COVID-19 Community-Based Testing Site",
+    address: "98 Coral Rd, San Francisco, CA 94107",
+    phone: "Phone Number Unavailable",
+    abbr: "communitySite",
+    appointment: "Required",
+    referral: "Not Required",
+    testing: "Tests Limited to Certain Patients",
+    drive: "Drive-Through Allowed",
+    lng: 37.754039,
+    lat: -122.399529,
+  },
+  {
+    siteName: "Forward",
+    address: "180 Sutter St, San Francisco, CA 94104",
+    phone: "833-334-6393",
+    abbr: "forward",
+    appointment: "Required",
+    referral: "Required",
+    testing: "Tests Limited to Certain Patients",
+    drive: "Drive-Through Not Allowed",
+    lng: 37.790108,
+    lat: -122.403676,
+  },
+  {
+    siteName: "One Medical",
+    address: "595 Castro St, San Francisco, CA 94114",
+    phone: "415-529-4099",
+    abbr: "oneMedical",
+    appointment: "Required",
+    referral: "Required",
+    testing: "Testing For All Patients",
+    drive: "Drive-Through Not Allowed",
+    lng: 37.759600,
+    lat: -122.434560,
+  },
+  {
+    siteName: "CVS",
+    address: "377 32nd Ave, San Francisco, CA 94121",
+    phone: "415-666-3153",
+    abbr: "cvs",
+    appointment: "Required",
+    referral: "Not Required",
+    testing: "Tests Limited to Certain Patients",
+    drive: "Drive-Through Allowed",
+    lng: 37.782184,
+    lat: -122.492915,
+  },
+  {
+    siteName: "San Francisco VA Medical Center",
+    address: "4150 Clement St, San Francisco, CA 94121",
+    phone: "415-221-4810",
+    abbr: "vaCenter",
+    appointment: "Required",
+    referral: "Required",
+    testing: "Tests Limited to Certain Patients",
+    drive: "Drive-Through Not Allowed",
+    lng: 37.782100,
+    lat: -122.505073,
+  },
+  {
+    siteName: "CityTest SF - Soma",
+    address: "600 7th St, San Francisco, CA 94103",
+    phone: "415-682-1740",
+    abbr: "cityTestSoma",
+    appointment: "Required",
+    referral: "Not Required",
+    testing: "Testing for All Patients",
+    drive: "Drive-Through Allowed",
+    lng: 37.772999,
+    lat: -122.403225,
+  },
+  {
+    siteName: "Castro-Mission Health Center Temporary Clinic",
+    address: "995 Potrero Ave Building 80, San Francisco, CA 94110",
+    phone: "415-682-1740",
+    abbr: "castroMission",
+    appointment: "Required",
+    referral: "Not Required",
+    testing: "Tests Limited to Certain Patients",
+    drive: "Drive-Through Not Allowed",
+    lng: 37.757429,
+    lat: -122.406165,
+  },
+  {
+    siteName: "CityTest SF - Pier 30/32",
+    address: "599 The Embarcadero, San Francisco, CA 94107",
+    phone: "415-701-2311",
+    abbr: "cityTestPier",
+    appointment: "Required",
+    referral: "Not Required",
+    testing: "Tests Limited to Certain Patients",
+    drive: "Drive-Through Allowed",
+    lng: 37.785876,
+    lat: -122.387516,
+  },
+  {
+    siteName: "Dignity Health-GoHealth Urgent Care",
+    address: "2288 Market St, San Francisco, CA 94114",
+    phone: "415-964-4855",
+    abbr: "dignity",
+    appointment: "Required",
+    referral: "Required",
+    testing: "Testing for All Patients",
+    drive: "Drive-Through Allowed",
+    lng: 37.764813,
+    lat: -122.432837,
+  },
+  {
+    siteName: "Carbon Health San Francisco-Castro",
+    address: "1998 Market St, San Francisco, CA 94102",
+    phone: "415-792-6040",
+    abbr: "carbonCastro",
+    appointment: "Not Required",
+    referral: "Not Required",
+    testing: "Tests Limited to Certain Patients",
+    drive: "Drive-Through Not Allowed",
+    lng: 37.770033,
+    lat: -122.426242,
+  },
+  {
+    siteName: "Sutter Urgent Care - San Francisco",
+    address: "55 2nd St Ground Floor, San Francisco, CA 94105",
+    phone: "415-600-6990",
+    abbr: "sutter",
+    appointment: "Required",
+    referral: "Not Required",
+    testing: "Testing for All Patients",
+    drive: "Drive-Through Not Allowed",
+    lng: 37.788900,
+    lat: -122.400313,
+  },
+  {
+    siteName: "Carbon Health Pop-Up Covid-19 Testing Center",
+    address: "470 Brannan St, San Francisco, CA 94107",
+    phone: "415-906-7643",
+    abbr: "carbonPop",
+    appointment: "Required",
+    referral: "Not Required",
+    testing: "Testing for All Patients",
+    drive: "Drive-Through Not Allowed",
+    lng: 37.778804,
+    lat: -122.396521,
+  },
+
+
+];
+
+const covidColor = 'yellow';
+
+$('#covidButton').on('click', function () {
+  for (let i = 0; i < covidArray.length; i++) {
+    if (covidArray.siteName !== '') {
+      let lat = covidArray[i].lat;
+      let lon = covidArray[i].lng;
+      let covid = covidArray[i].siteName
+      let covidAddress = covidArray[i].address;
+      let covidPhone = covidArray[i].phone;
+      let covidAppointment = covidArray[i].appointment;
+      let covidReferral = covidArray[i].referral;
+      let covidTesting = covidArray[i].testing;
+      let covidDrive = covidArray[i].drive;
+    L.marker([lon, lat]).addTo(mymap1).bindPopup(`
+      <strong>${covid}</strong>
+      <br>
+      ${covidAddress}
+      <br>
+      ${covidPhone}
+      <br>
+      ${"Appointment" + " " + covidAppointment}
+      <br>
+      ${"Referral" + " " + covidReferral}
+      <br>
+      ${covidTesting}
+      <br>
+      ${covidDrive}
+    `);
+    }
+  }
 });
